16dfe33aec41eeb035cc65f676fda8044bc63c29
atmatm6 <atmatm6000@gmail.com>
2016-08-08 11:14:10 -0600

    Modify TestPlug
    fix intellij inspection errors

--- src/main/java/atmatm6/testplug/TestPlug.java
+++ src/main/java/atmatm6/testplug/TestPlug.java
@@ -1,5 +1,6 @@
 package atmatm6.testplug;
 
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class TestPlug extends Plugin {
@@ -7,7 +8,12 @@ public class TestPlug extends Plugin {
     public void onEnable() {
         // You should not put an enable message in your plugin.
         // BungeeCord already does so
-        getLogger().info(getProxy().getServers().get("creative").getAddress().toString());
-
+        //noinspection Since15
+        getProxy().getServers().forEach(new java.util.function.BiConsumer<String, ServerInfo>() {
+            @Override
+            public void accept(String s, ServerInfo serverInfo) {
+                getLogger().info(serverInfo.getAddress().toString());
+            }
+        });
     }
 }
