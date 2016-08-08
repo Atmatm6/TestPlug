9b402a434e3f7eec796b6b7c28785a3f824acad0
atmatm6 <atmatm6000@gmail.com>
2016-08-08 10:18:21 -0600

    Initial commit

---
+++ src/main/java/atmatm6/testplug/TestPlug.java
@@ -0,0 +1,12 @@
+package atmatm6.testplug;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class TestPlug extends Plugin {
+    @Override
+    public void onEnable() {
+        // You should not put an enable message in your plugin.
+        // BungeeCord already does so
+        getLogger().info("Yay! It loads!");
+    }
+}
